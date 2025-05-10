7b269ecf6f2209f8925141de2e44f6a695398328
pnm <null>
2025-05-10 14:56:25 +0400

    Delete scheme.sql, because it is not required yet

--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `Users` (
-    `id` int AUTO_INCREMENT NOT NULL,
-    `login` VARCHAR(32) NOT NULL,
-    `password` VARCHAR(32) NOT NULL,
-    `privileges` int NOT NULL,
-    PRIMARY KEY (`id`)
-);
